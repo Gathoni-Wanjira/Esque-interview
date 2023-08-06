8ff06bb03b80e1d6b68e1db56f44c445e592920e
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-08-06 22:46:46 +0300

    Side bar styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-ui">
+
+
+        <div className='Sidebar'>
+
+
+        <i className="fa-regular fa-snowflake" id = 'onlyicon' ></i>
+        <div className='sideContent'id ='dashtext'>
+        <i className="fa-brands fa-windows windows" ></i>
+        <p>Dashboard</p>
+        </div>
+        <div className='sideContent'>
+        <i className="fa-brands fa-windows windows" ></i>
+        <p>Customers</p>
+        </div>
+        <div className='sideContent'>
+        <i className="fa-brands fa-windows windows" ></i>
+        <p>Orders</p>
+        </div>
+        <div className='sideContent'>
+        <i className="fa-brands fa-windows windows" ></i>
+        <p>Settings</p>
+        </div>
+        <div className='sideContent'>
+        <i className="fa-brands fa-windows windows" ></i>
+        <p>Finance</p>
+        </div>
+        <div className='sideContent'>
+        <i className="fa-brands fa-windows windows" ></i>
+        <p>Stock</p>
+        </div>
+        <div className='sideContent'>
+        <i className="fa-brands fa-windows windows" ></i>
+        <p>Driver</p>
+        </div>
+        
+        </div>
+
+
+        <div className='main-content'>
+            <div className='Navbar'>
+                <div className='welcome'>
+                <p className = "User">Hello Gathoni <i className="fa-solid fa-face-smile" ></i> </p>
+           <p className='compliment'>Happy to see you back!</p>
+
+                </div>
+            
+                <div className='searchbar'>
+                <i className="fa-regular fa-magnifying-glass" id = "searchIcon" ></i>
+                <input type='text' placeholder='search'/>
+                
+                </div>
+            
+
+           
+
+
+            </div>
+
+        </div>
+     
     </div>
   );
 }
