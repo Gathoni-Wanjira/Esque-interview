b5c67130703d0660408411f19fd500f1114f910a
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-08-07 22:34:35 +0300

    Customers List

--- src/App.js
+++ src/App.js
@@ -1,69 +1,12 @@
-import './App.css';
+import "./App.css";
+import Homepage from "./Homepage";
 
 function App() {
   return (
-    <div className="App-ui">
-
-
-        <div className='Sidebar'>
-
-
-        <i className="fa-regular fa-snowflake" id = 'onlyicon' ></i>
-        <div className='sideContent'id ='dashtext'>
-        <i className="fa-brands fa-windows windows" ></i>
-        <p>Dashboard</p>
-        </div>
-        <div className='sideContent'>
-        <i className="fa-brands fa-windows windows" ></i>
-        <p>Customers</p>
-        </div>
-        <div className='sideContent'>
-        <i className="fa-brands fa-windows windows" ></i>
-        <p>Orders</p>
-        </div>
-        <div className='sideContent'>
-        <i className="fa-brands fa-windows windows" ></i>
-        <p>Settings</p>
-        </div>
-        <div className='sideContent'>
-        <i className="fa-brands fa-windows windows" ></i>
-        <p>Finance</p>
-        </div>
-        <div className='sideContent'>
-        <i className="fa-brands fa-windows windows" ></i>
-        <p>Stock</p>
-        </div>
-        <div className='sideContent'>
-        <i className="fa-brands fa-windows windows" ></i>
-        <p>Driver</p>
-        </div>
-        
-        </div>
-
-
-        <div className='main-content'>
-            <div className='Navbar'>
-                <div className='welcome'>
-                <p className = "User">Hello Gathoni <i className="fa-solid fa-face-smile" ></i> </p>
-           <p className='compliment'>Happy to see you back!</p>
-
-                </div>
-            
-                <div className='searchbar'>
-                <i className="fa-regular fa-magnifying-glass" id = "searchIcon" ></i>
-                <input type='text' placeholder='search'/>
-                
-                </div>
-            
-
-           
-
-
-            </div>
-
-        </div>
-     
-    </div>
+    <>
+    <Homepage />
+    </>
+   
   );
 }
 
